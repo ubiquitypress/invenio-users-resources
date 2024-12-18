79bfb770821842b466b509fd5cdc29ec8d06fc8a
Jacob Collins <jacob.collins@ubiquitypress.com>
2024-12-18 15:19:33 +0000

    REPO-2586: added copyright

--- invenio_users_resources/resources/groups/resource.py
+++ invenio_users_resources/resources/groups/resource.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2022 TU Wien.
 # Copyright (C) 2022 CERN.
 # Copyright (C) 2022 European Union.
+# Copyright (C) 2024 Ubiquity Press.
 #
 # Invenio-Users-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
