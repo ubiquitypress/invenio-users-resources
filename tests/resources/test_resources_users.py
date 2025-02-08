4417f6cfb63306d76e790db9f1957b88b7ebd965
Jacob Collins <jacob.collins@ubiquitypress.com>
2025-02-08 15:09:48 +0000

    REPO-2531: Initial functionality to stop superadmin Role being seen or edited by non superadmin users.

--- tests/resources/test_resources_users.py
+++ tests/resources/test_resources_users.py
@@ -278,7 +278,7 @@ def test_role_management_for_user(
         "hits": {
             "hits": [
                 {
-                    "description": "Admin group",
+                    "description": "user_management_action group",
                     "id": "administration-moderation",
                     "name": "administration-moderation",
                 }
